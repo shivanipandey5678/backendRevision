93c0e224df4b150e30dfff219e81f3f49de66a01
Shivani <shivanipandey0107@gmail.com>
2024-04-08 15:00:00 +0530

    working on blogapp made schema,start server,starting with model of blog and user

--- blogApp/index.js
+++ blogApp/index.js
@@ -1,8 +1,20 @@
+const mongoose=require("mongoose");
+const connection=async()=>{
+    await mongoose.connect("mongodb://127.0.0.1:27017/blogApp")
+    .then(()=>{console.log("Connected to db bro")})
+    .catch(()=>{console.log("Error in connection")});
+}
+
 const express=require("express");
 const app=express();
 require('dotenv').config()
 const PORT=process.env.PORT;
 
-app.listen(PORT,()=>{
+
+app.get("/health",(req,res)=>{
+    res.send("connnected")
+})
+app.listen(PORT,async()=>{
+    await connection()
     console.log(`listening to port : ${PORT}`)
 })
