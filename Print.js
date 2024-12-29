0f88bdd43998eb542b986f9237ef52ff4c850cda
Shivani <shivanipandey0107@gmail.com>
2024-12-29 10:00:00 +0530

    starting backend

---
+++ Print.js
@@ -0,0 +1,5 @@
+const printName=(name)=>{
+   console.log(name)
+}
+
+module.exports=printName;
---
+++ day1.js
@@ -0,0 +1,7 @@
+const sum=require("./sum");
+let printName=require("./Print");
+
+printName("shiv")
+
+
+console.log(sum(1,2));
